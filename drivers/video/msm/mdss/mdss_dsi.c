41cd06942aa72e8daab26805e38e73f0fd3402ac
FacuM <facumo.fm@gmail.com>
2017-10-29 00:48:18 -0300

    Fix video driver MDSS compilation.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -25,8 +25,6 @@
 #include <linux/clk.h>
 #include <linux/uaccess.h>
 #include <linux/leds.h>
-#include <linux/lcd_notify.h>
-
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -79,6 +77,7 @@ static int mdss_dsi_regulator_init(struct platform_device *pdev)
 			}
 		}
 	}
+
 	return rc;
 }
 
@@ -603,15 +602,7 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 	 * clocks.
 	 */
 	mdss_dsi_clk_ctrl(ctrl_pdata, DSI_BUS_CLKS, 1);
-
-	/*
-	 * If ULPS during suspend feature is enabled, then DSI PHY was
-	 * left on during suspend. In this case, we do not need to reset/init
-	 * PHY. This would have already been done when the BUS clocks are
-	 * turned on. However, if cont splash is disabled, the first time DSI
-	 * is powered on, phy init needs to be done unconditionally.
-	 */
-	if (!pdata->panel_info.ulps_suspend_enabled || !ctrl_pdata->ulps) {
+	if (!pdata->panel_info.ulps_suspend_enabled) {
 		mdss_dsi_phy_sw_reset(ctrl_pdata);
 		mdss_dsi_phy_init(ctrl_pdata);
 		mdss_dsi_ctrl_setup(ctrl_pdata);
@@ -1278,14 +1269,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
-
 		if (ctrl_pdata->pre_on_cmds.cmd_cnt &&
 			ctrl_pdata->pre_on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_pre_unblank(pdata);
-		mdss_dsi_get_hw_revision(ctrl_pdata);
-
-		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
-
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc |= mdss_dsi_unblank(pdata);
 		break;
@@ -1296,10 +1282,8 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.esd_rdy = true;
-		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
-		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		power_state = (int) (unsigned long) arg;
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
@@ -1310,7 +1294,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
 		rc = mdss_dsi_off(pdata, power_state);
-		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
@@ -1355,6 +1338,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 	case MDSS_EVENT_REGISTER_RECOVERY_HANDLER:
 		rc = mdss_dsi_register_recovery_handler(ctrl_pdata,
 			(struct mdss_intf_recovery *)arg);
+		break;
+	case MDSS_EVENT_INTF_RESTORE:
+		mdss_dsi_ctrl_phy_restore(ctrl_pdata);
 		break;
 	default:
 		pr_debug("%s: unhandled event=%d\n", __func__, event);
@@ -1627,10 +1613,6 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 		goto error_pan_node;
 	}
 
-	ctrl_pdata->cmd_clk_ln_recovery_en =
-		of_property_read_bool(pdev->dev.of_node,
-			"qcom,dsi-clk-ln-recovery");
-
 	if (mdss_dsi_is_te_based_esd(ctrl_pdata)) {
 		rc = devm_request_irq(&pdev->dev,
 			gpio_to_irq(ctrl_pdata->disp_te_gpio),
@@ -1646,7 +1628,8 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 	return 0;
 
 error_pan_node:
-	mdss_dsi_unregister_bl_settings(ctrl_pdata);
+	if (BL_WLED == ctrl_pdata->bklt_ctrl)
+		led_trigger_unregister_simple(bl_led_trigger);
 	of_node_put(dsi_pan_node);
 error_vreg:
 	for (i = DSI_MAX_PM - 1; i >= 0; i--)
@@ -2031,8 +2014,7 @@ int dsi_panel_device_register(struct device_node *pan_node,
 			pr_err("%s: Panel power on failed\n", __func__);
 			return rc;
 		}
-		if (ctrl_pdata->bklt_ctrl == BL_PWM)
-			ctrl_pdata->pwm_enabled = 1;
+
 		pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 		mdss_dsi_clk_ctrl(ctrl_pdata, DSI_ALL_CLKS, 1);
 		ctrl_pdata->ctrl_state |=
