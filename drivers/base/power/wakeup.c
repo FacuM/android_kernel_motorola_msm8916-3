302834df8fb5a0b4fe3f9730eba457470a8613b1
FacuM <facumo.fm@gmail.com>
2017-10-30 00:06:04 -0300

    Improve wakelocks toggles, add one more.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -30,6 +30,8 @@ static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
 static bool enable_bluesleep_ws = true;
 module_param(enable_bluesleep_ws, bool, 0644);
+static bool enable_msm_hsic_ws = true;
+module_param(enable_msm_hsic_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -423,6 +425,21 @@ EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
+
+         if (!enable_wlan_rx_wake_ws && !strcmp(ws->name, "wlan_rx_wake"))
+                 return;
+
+ 	if (!enable_wlan_ctrl_wake_ws && !strcmp(ws->name, "wlan_ctrl_wake"))
+                 return;
+
+  	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
+                  return;
+
+        if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host")) {
+                pr_info("wakeup source msm_hsic_host activate skipped\n");
+                return;
+         }
+
 
 	/*
 	 * active wakeup source should bring the system
