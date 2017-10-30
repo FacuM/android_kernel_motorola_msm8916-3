bb2e90c14b3bf0a1109e25315cbe82c290df0775
FacuM <facumo.fm@gmail.com>
2017-10-30 01:56:04 -0300

    Revert "Revert "Remove modules support.""
    This reverts commit 611283505396a63c56b182f8e0062fa8ce8d1599.

--- build_cwm_zip.sh
+++ build_cwm_zip.sh
@@ -17,13 +17,8 @@ else
  rm -f "$ZIPPATH/boot.img"
  make ARCH=arm -j10 zImage
  make ARCH=arm -j10 dtimage
- make ARCH=arm -j10 modules
  rm -rf squid_install
  mkdir -p squid_install
- make ARCH=arm -j10 modules_install INSTALL_MOD_PATH=squid_install INSTALL_MOD_STRIP=1
- mkdir -p "$ZIPPATH/system/lib/modules/pronto"
- find squid_install/ -name '*.ko' -type f -exec cp '{}' "$ZIPPATH/system/lib/modules/" \;
- mv "$ZIPPATH/system/lib/modules/wlan.ko" "$ZIPPATH/system/lib/modules/pronto/pronto_wlan.ko"
  cp arch/arm/boot/zImage "$ZIPPATH/tools/"
  cp arch/arm/boot/dt.img "$ZIPPATH/tools/"
  VERSION=$(cat Makefile | grep "EXTRAVERSION = -" | sed 's/EXTRAVERSION = -//')
